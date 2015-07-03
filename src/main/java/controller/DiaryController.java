44cc667f7d6b878227d6c084caac2aaf967791db
윤동혁 <ydh21015@naver.com>
2015-07-03 16:25:04 +0900

    no message

--- src/main/java/controller/DiaryController.java
+++ src/main/java/controller/DiaryController.java
@@ -28,14 +28,8 @@ import service.WmpService;
 public class DiaryController {
 	@Autowired
 	private WmpService wmpService;
-<<<<<<< HEAD
 
 //	장소 검색
-=======
-	
-	
-	
->>>>>>> YDH
 	@RequestMapping(value="searchPlace", method=RequestMethod.GET)
 	public String searchPlace(HttpServletRequest req, HttpServletResponse rep, Model model) throws IOException{
 
